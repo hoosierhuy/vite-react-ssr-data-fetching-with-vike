ca1553ccd6bd9975d65c630bb20e2c908ebb84b2
Huy Nguyen <huynguyen@MacBookPro.lan>
2025-01-02 18:33:03 -0500

    Reformatted files to make them easier to read

--- renderer/Link.tsx
+++ renderer/Link.tsx
@@ -2,11 +2,18 @@ import { usePageContext } from './usePageContext'
 
 export { Link }
 
-function Link(props: { href: string; className?: string; children: React.ReactNode }) {
+function Link(props: {
+  href: string
+  className?: string
+  children: React.ReactNode
+}) {
   const pageContext = usePageContext()
   const { urlPathname } = pageContext
   const { href } = props
-  const isActive = href === '/' ? urlPathname === href : urlPathname.startsWith(href)
-  const className = [props.className, isActive && 'is-active'].filter(Boolean).join(' ')
+  const isActive =
+    href === '/' ? urlPathname === href : urlPathname.startsWith(href)
+  const className = [props.className, isActive && 'is-active']
+    .filter(Boolean)
+    .join(' ')
   return <a {...props} className={className} />
 }
--- renderer/usePageContext.tsx
+++ renderer/usePageContext.tsx
@@ -6,9 +6,17 @@ export { PageContextProvider }
 import React, { useContext } from 'react'
 import type { PageContext } from 'vike/types'
 
-const Context = React.createContext<PageContext>(undefined as unknown as PageContext)
+const Context = React.createContext<PageContext>(
+  undefined as unknown as PageContext
+)
 
-function PageContextProvider({ pageContext, children }: { pageContext: PageContext; children: React.ReactNode }) {
+function PageContextProvider({
+  pageContext,
+  children,
+}: {
+  pageContext: PageContext
+  children: React.ReactNode
+}) {
   return <Context.Provider value={pageContext}>{children}</Context.Provider>
 }
 
